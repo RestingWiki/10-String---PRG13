f3358ea47dfb97d917c3d8d2bd44e14c89cf6fd8
RestingWiki <126898951+RestingWiki@users.noreply.github.com>
2023-03-25 21:28:21 +0700

    Add project files.

---
+++ Project1/Header.h
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+#ifndef HEADER_H
+#define HEADER_H
+
+class convertTo {
+
+private:
+	string input, base;
+
+public:
+	///////////////// 
+	// Constructor //
+	/////////////////
+
+	convertTo();						// Default constructor
+	convertTo(string i, string b);		// Param constructor
+
+	// Accessor , Mutator
+	void changeType(string i, string b);
+
+	void getDecimal();
+	void getOctal();
+	void getBinary();
+	void getHexa();
+
+private:
+	void toDecimal();
+	void toOctal();
+	void toBinary();
+	void toHexa();
+
+
+};
+
+#endif // !HEADER_H
+
